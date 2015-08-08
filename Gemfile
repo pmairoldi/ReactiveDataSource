6deac97b8e7fc580cc6475dd0de6a1baf49d6c24
Pierre-Marc Airoldi <pierremarcairoldi@gmail.com>
2015-08-08 15:02:16 -0400

    add code coverage

--- Gemfile
+++ Gemfile
@@ -2,3 +2,4 @@ source "https://rubygems.org"
 
 gem 'net-ssh', '2.9'
 gem 'fastlane', '~> 1.15'
+gem 'slather', :git => 'https://github.com/petester42/slather.git'
--- UnitTests/ReactiveCocoaExtensionTests.swift
+++ UnitTests/ReactiveCocoaExtensionTests.swift
@@ -12,10 +12,10 @@ class ReactiveCocoaExtensionTests: XCTestCase {
     
     class MockViewModel: Reusable {
         var reuseIdentifier: String {
-            return "reuseId"
+            return ""
         }
     }
-    
+
     override func setUp() {
         super.setUp()
         // Put setup code here. This method is called before the invocation of each test method in the class.
