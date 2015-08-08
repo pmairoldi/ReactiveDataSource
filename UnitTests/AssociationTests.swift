68b8bcf2e76ddcd95a2b19091b4bd8d0d14ec413
Pierre-Marc Airoldi <pierremarcairoldi@gmail.com>
2015-08-08 01:39:50 -0400

    add tests for extensions and data

---
+++ UnitTests/AssociationTests.swift
@@ -0,0 +1,28 @@
+import XCTest
+@testable import ReactiveDataSource
+
+class AssociationTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+
+    func testAssociatedObject() {
+        
+        class Object: NSObject {
+            private static var key: UInt8 = 0
+        }
+        
+        let object = Object()
+        
+        associatedObject(object, key: &Object.key, initial: { "initial" })
+        
+        XCTAssertEqual(objc_getAssociatedObject(object, &Object.key) as? String, "initial")
+    }
+}
